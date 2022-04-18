0013a3fc4eaee88c92a17773d6914e4391100117
Inok1308 <henokmas@gmail.com>
2022-04-18 16:04:40 -0700

    all

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,5 +1,10 @@
 #include "main.h"
-void reser (int *n)
+/**
+ * reset_to_98 - sets the value of an int pointer to 98
+ * @n: The int pointer to reset
+ */
+void reset_to_98(int *n)
 {
-*n=98;
+*n = 98;
+b10_pow(10);
 }
